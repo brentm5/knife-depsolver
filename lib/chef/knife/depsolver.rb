227e6bccb80f2e876798c475789b428a6909c49d
Jeremiah Snapp <jeremiah@chef.io>
2016-03-17 17:07:44 -0400

    Add error handling

--- lib/chef/knife/depsolver.rb
+++ lib/chef/knife/depsolver.rb
@@ -1,13 +1,4 @@
-# This `knife exec` script gets a node's expanded run_list with version
-# constraints for a specified environment.
-
-# Then it POSTs the expanded run_list to the environment's cookbook_versions
-# endpoint to retrieve a list of dependency resolved cookbooks that meet all
-# specified version constraints.
-
 require 'chef/knife'
-require 'chef/run_list/run_list_expansion'
-require 'chef/run_list/run_list_item'
 
 class Chef
   class Knife
@@ -23,52 +14,63 @@ class Chef
              description: 'Use the run list from a given node'
 
       def run
-        cookbook_versions = {}
-
-        run_list = name_args.map {|item| item.to_s.split(/,/) }.flatten.each{|item| item.strip! }
-        run_list.delete_if {|item| item.empty? }
-
-        if config[:node]
-          node = Chef::Node.load(config[:node])
-        else
-          node = Chef::Node.new
-          node.name('depsolver-tmp-node')
-          run_list.each do |arg|
-            node.run_list.add(arg)
-          end
-        end
-
-        environment = config[:environment]
-        environment ||= node.chef_environment
-
-        run_list_expansion = node.expand!
-        expanded_run_list_with_versions = run_list_expansion.recipes.with_version_constraints_strings
-
         begin
+          if config[:node]
+            node = Chef::Node.load(config[:node])
+          else
+            node = Chef::Node.new
+            node.name('depsolver-tmp-node')
+
+            run_list = name_args.map {|item| item.to_s.split(/,/) }.flatten.each{|item| item.strip! }
+            run_list.delete_if {|item| item.empty? }
+
+            run_list.each do |arg|
+              node.run_list.add(arg)
+            end
+          end
+
+          environment = config[:environment]
+          environment ||= node.chef_environment
+
+          environment_cookbook_versions = Chef::Environment.load(environment).cookbook_versions
+
+          run_list_expansion = node.expand!
+          expanded_run_list_with_versions = run_list_expansion.recipes.with_version_constraints_strings
+
           depsolver_start_time = Time.now
+
           ckbks = rest.post_rest("environments/" + environment + "/cookbook_versions", { "run_list" => expanded_run_list_with_versions })
+
           depsolver_finish_time = Time.now
-        rescue
+
+          depsolver_results = {}
+          ckbks.each do |name, ckbk|
+            version = ckbk.is_a?(Hash) ? ckbk['version'] : ckbk.version
+            depsolver_results[name] = version
+          end
+
+        rescue => e
+          api_error = {}
+          api_error[:error_code] = e.response.code
+          api_error[:error_message] = e.response.message
+          begin
+            api_error[:error_body] = JSON.parse(e.response.body)
+          rescue JSON::ParserError
+          end
+        ensure
+          results = {}
+          results[:node] = node.name unless node.nil? || node.name.nil?
+          results[:environment] = environment unless environment.nil?
+          results[:environment_cookbook_versions] = environment_cookbook_versions unless environment_cookbook_versions.nil?
+          results[:run_list] = node.run_list unless node.nil? || node.run_list.nil?
+          results[:expanded_run_list] = expanded_run_list_with_versions unless expanded_run_list_with_versions.nil?
+          results[:depsolver_results] = depsolver_results unless depsolver_results.nil? || depsolver_results.empty?
+          results[:depsolver_cookbook_count] = ckbks.count unless ckbks.nil?
+          results[:depsolver_elapsed_ms] = ((depsolver_finish_time - depsolver_start_time) * 1000).to_i unless depsolver_finish_time.nil?
+          results[:api_error] = api_error unless api_error.nil?
+
+          msg(JSON.pretty_generate(results))
         end
-
-        depsolver_results = {}
-        ckbks.each do |name, ckbk|
-          version = ckbk.is_a?(Hash) ? ckbk['version'] : ckbk.version
-          depsolver_results[name] = version
-        end
-
-        results = {
-          node: node.name,
-          environment: environment,
-          environment_cookbook_versions: Chef::Environment.load(environment).cookbook_versions,
-          run_list: node.run_list,
-          expanded_run_list: expanded_run_list_with_versions,
-          depsolver_results: depsolver_results,
-          depsolver_cookbook_count: depsolver_results.count,
-          depsolver_elapsed_ms: ((depsolver_finish_time - depsolver_start_time) * 1000).to_i
-        }
-
-        msg(JSON.pretty_generate(results))
       end
     end
   end
